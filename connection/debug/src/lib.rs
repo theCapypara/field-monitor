a761e9a93f19960515fd7aa006b8e4e15fb05e02
Marco "Capypara" Köpcke <hello@capypara.de>
2025-03-08 17:37:02 +0100

    Copyright update 2025

--- connection/debug/src/lib.rs
+++ connection/debug/src/lib.rs
@@ -1,4 +1,4 @@
-/* Copyright 2024 Marco Köpcke
+/* Copyright 2024-2025 Marco Köpcke
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
